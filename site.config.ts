f2bb7261ee0a47c3bc15e23d4fd5c60b87605c34
neverdice <90811593+neverdice@users.noreply.github.com>
2023-10-08 12:03:33 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f126c4a06cef47f1815495d0450f618a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'https://www.notion.so/f126c4a06cef47f1815495d0450f618a',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
